bb2767e6d5308d553b0b7905023d317204063820
Ajay Kumar <official.ajaykumar94@gmail.com>
2025-04-19 15:03:53 +0530

    emailverifier done

--- emailverifier-go/main.go
+++ emailverifier-go/main.go
@@ -6,12 +6,21 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
 type EmailResponse struct {
-	IsValid bool `json:"isValid"`
+	Status      string `json:"status"`
+	Domain      string `json:"domain"`
+	HasMx       bool   `json:"hasMx"`
+	IsValid     bool   `json:"isValid"`
+	HasSPF      bool   `json:"hasSPF"`
+	SpfRecord   string `json:"spfRecord"`
+	HasDMARC    bool   `json:"hasDMARC"`
+	DmarcRecord string `json:"dmarcRecord"`
+	Description string `json:"description"`
 }
 
 func main() {
@@ -25,18 +34,60 @@ func main() {
 
 func EmailValidator(w http.ResponseWriter, r *http.Request) {
 
+	hasspf := false
+	hasdmarc := false
+	isValid := false
+	hasMx := false
+	description := ""
+	spfRecord := ""
+	dmarcRecord := ""
 	params := mux.Vars(r)
 
 	emailId := params["emailId"]
 	w.Header().Set("Content-Type", "application/json")
 
 	if emailId == "" {
-		json.NewEncoder(w).Encode(EmailResponse{IsValid: false})
+		json.NewEncoder(w).Encode(EmailResponse{Status: "Success", IsValid: false})
+		return
 	}
 
-	if resp, err := net.LookupTXT(emailId); len(resp) == 0 && err != nil {
-		json.NewEncoder(w).Encode(EmailResponse{IsValid: false})
+	mxr, err := net.LookupMX(emailId)
+	if len(mxr) > 0 {
+		hasMx = true
 	}
-	json.NewEncoder(w).Encode(EmailResponse{IsValid: true})
-	fmt.Println("Email is valid")
+
+	resp, err := net.LookupTXT(emailId)
+
+	if err != nil {
+		fmt.Println("Email is not valid")
+		json.NewEncoder(w).Encode(EmailResponse{Status: "Success", IsValid: false, HasSPF: hasspf, HasDMARC: hasdmarc})
+		return
+	}
+
+	for _, record := range resp {
+		isValid = true
+		if strings.HasPrefix(record, "v=spf1") {
+			fmt.Println("SPF record found")
+			hasspf = true
+			spfRecord = record
+			description = description + record + "\n"
+			break
+		}
+	}
+
+	dmarcResp, _ := net.LookupTXT("_dmarc." + emailId)
+
+	if len(dmarcResp) > 0 {
+		for _, record := range dmarcResp {
+			if strings.HasPrefix(record, "v=DMARC1") {
+				fmt.Println("DMARC record found")
+				hasdmarc = true
+				dmarcRecord = record
+				description = description + record + "\n"
+				break
+			}
+		}
+	}
+
+	json.NewEncoder(w).Encode(EmailResponse{Status: "Success", Domain: emailId, IsValid: isValid, HasMx: hasMx, HasSPF: hasspf, SpfRecord: spfRecord, HasDMARC: hasdmarc, DmarcRecord: dmarcRecord, Description: description})
 }
