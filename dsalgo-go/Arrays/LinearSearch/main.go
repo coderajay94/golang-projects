830ff542960112e901ff4914435eec97e041eca9
Ajay Kumar <official.ajaykumar94@gmail.com>
2025-05-14 23:17:21 +0530

    Linear search with range based

--- dsalgo-go/Arrays/LinearSearch/main.go
+++ dsalgo-go/Arrays/LinearSearch/main.go
@@ -10,10 +10,13 @@ func main() {
 	fmt.Println("Hello World from Linear Search!!")
 
 	elements := [9]int{12, 14, 1, 34, 61, 88, 56, -11, 11}
+	elementsSlice := []int{14, 1, 34, 61, 88, 56, -11, 11}
 
-	fmt.Println("returns index:", LinearSearch(elements, 11))
-	fmt.Println("returns element if exists:", LinearSearch2(elements, 111))
-	fmt.Println("returns bool:", LinearSearch3(elements, 11))
+	fmt.Println("Search returns index:", LinearSearch(elements, 11))
+	fmt.Println("Search returns element if exists:", LinearSearch2(elements, 111))
+	fmt.Println("Search returns bool:", LinearSearch3(elements, 11))
+	index, found := LinearSearch4(elementsSlice, 11, 3, 7)
+	fmt.Println("Searches between range returns found:", found, "at ", index)
 
 }
 
@@ -64,4 +67,23 @@ func LinearSearch3(elements [9]int, item int) bool {
 
 	return false
 
+}
+
+func LinearSearch4(elements []int, target int, start int, end int) (int, bool) {
+
+	if len(elements) == 0 || end > len(elements)-1 || start < 0 || start > end {
+		return -1, false
+	}
+
+	//end+1 as end is not inclusive start to end-1 would be returned otherwise
+	elements = elements[start : end+1]
+	fmt.Println("updated slice", elements)
+
+	for index, element := range elements {
+		if element == target {
+			//return index of the range start array
+			return start + index, true
+		}
+	}
+	return -1, false
 }
