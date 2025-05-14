87f659556deb46ca7d9a1297c112cab0478ba087
Ajay Kumar <official.ajaykumar94@gmail.com>
2025-05-14 20:37:43 +0530

    Linear search in golang

--- dsalgo-go/Arrays/main.go
+++ dsalgo-go/Arrays/main.go
@@ -6,13 +6,13 @@ func main() {
 
 	fmt.Println("Hello World from Linear Search!!")
 
-	elements := []int{12, 14, 1, 34, 61, 88, 56, 11}
+	elements := [9]int{12, 14, 1, 34, 61, 88, 56, -11, 11}
 
 	fmt.Println(LinearSearch(elements, 11))
 
 }
 
-func LinearSearch(elements []int, item int) int {
+func LinearSearch(elements [9]int, item int) int {
 	if len(elements) == 0 {
 		return -1
 	}
